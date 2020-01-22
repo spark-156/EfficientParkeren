e482dd963dd44d07c66085ced63d26a9918b7f7a
spark-156 <57094525+spark-156@users.noreply.github.com>
2020-01-22 10:24:46 +0100

    Raspberry sensoren backup

--- master.py
+++ master.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-'oke'
 for bitch in here:
     aheroqihweo
-=======
+
 import datetime
 datetime = datetime.datetime.now()
->>>>>>> 5d1b1d077bbda475f06c6f348204caefd84b844a
 
 def ingaanstijd():
     sql_command = '''CREATE TABLE ingaanstijd (ID INTEGER PRIMARY KEY, parkeervak CHAR(1), datum = datetime, tijd = datetime);'''

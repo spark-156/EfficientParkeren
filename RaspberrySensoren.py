9f8385b77f9d5d5981ba75be845877259f949603
Daanvb2001 <Daan.vanbijsterveld@student.hu.nl>
2020-01-23 11:35:14 +0100

    Update RaspberrySensoren.py

--- RaspberrySensoren.py
+++ RaspberrySensoren.py
@@ -7,6 +7,12 @@ GPIO.setmode(GPIO.BCM)
 # set GPIO Pins
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
+GPIO_TRIGGER2 = 20
+GPIO_ECHO2 = 21
+
+blue2 = 26
+green2 = 19
+red2 = 13
 
 blue = 17
 green = 27
@@ -22,12 +28,21 @@ GPIO.output(green, GPIO.LOW)
 GPIO.setup(blue, GPIO.OUT)
 GPIO.output(blue, GPIO.LOW)
 
+GPIO.setup(GPIO_TRIGGER2, GPIO.OUT)
+GPIO.setup(GPIO_ECHO2, GPIO.IN)
+GPIO.setup(red2, GPIO.OUT)
+GPIO.output(red2, GPIO.LOW)
+GPIO.setup(green2, GPIO.OUT)
+GPIO.output(green2, GPIO.LOW)
+GPIO.setup(blue2, GPIO.OUT)
+GPIO.output(blue2, GPIO.LOW)
 
-def distance():
+
+def distance1():
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
 
-    time.sleep(0.00001)
+    time.sleep(0.001)
     GPIO.output(GPIO_TRIGGER, False)
 
     # set Trigger after 0.01ms to LOW
@@ -46,14 +61,42 @@ def distance():
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+    distance1 = (TimeElapsed * 34300) / 2
 
-    return distance
+    return distance1
+
+
+def distance2():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER2, True)
+
+    time.sleep(0.001)
+    GPIO.output(GPIO_TRIGGER2, False)
+
+    # set Trigger after 0.01ms to LOW
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO2) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO2) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance2 = (TimeElapsed * 34300) / 2
+
+    return distance2
 
 
 try:
     while True:
-        dist = distance()
+        dist = distance1()
         print("Measured Distance = %.1f cm" % dist)
         if dist > 25:
             GPIO.output(blue, GPIO.LOW)
@@ -67,8 +110,23 @@ try:
             GPIO.output(green, GPIO.LOW)
             GPIO.output(red, GPIO.LOW)
             GPIO.output(blue, GPIO.HIGH)
-        time.sleep(1)
+        time.sleep(2)
 
+        dist2 = distance2()
+        print("Gemeten afstand = %.1f cm" % dist2)
+        if dist2 > 25:
+            GPIO.output(blue2, GPIO.LOW)
+            GPIO.output(red2, GPIO.LOW)
+            GPIO.output(green2, GPIO.HIGH)
+        elif dist2 < 20:
+            GPIO.output(green2, GPIO.LOW)
+            GPIO.output(blue2, GPIO.LOW)
+            GPIO.output(red2, GPIO.HIGH)
+        elif dist2 > 20 and dist2 < 25:
+            GPIO.output(green2, GPIO.LOW)
+            GPIO.output(red2, GPIO.LOW)
+            GPIO.output(blue2, GPIO.HIGH)
+        time.sleep(2)
 
 except KeyboardInterrupt:
     print("Measurement stopped by User")
